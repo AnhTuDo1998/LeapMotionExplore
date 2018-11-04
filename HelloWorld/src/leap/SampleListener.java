60ad3f1e3a7c39a284dd7637658908e80da95bde
Do Anh Tu <32509869+AnhTuDo1998@users.noreply.github.com>
2018-11-04 23:15:41 +0800

    Fixed false hand detect. Only print out if left or right hand is detected

--- HelloWorld/src/leap/SampleListener.java
+++ HelloWorld/src/leap/SampleListener.java
@@ -14,33 +14,39 @@ public class SampleListener extends Listener{
 		 //get a frame
 		 Frame frame = controller.frame();
 		 HandList hands = frame.hands();
-		 
-		 Hand firstHand = hands.get(0);
-		 
-		//get fingers
-		 FingerList fingers = firstHand.fingers();
-		 
-		 //get position, velocity and direction
-		 //Vector position = firstHand.palmPosition();
-		 //Vector velocity = firstHand.palmVelocity();
-		 //Vector direction = firstHand.direction();
-		 
-		 float pitch = firstHand.direction().pitch();
-		 float yaw = firstHand.direction().yaw();
-		 float roll = firstHand.palmNormal().roll();
-		 if (firstHand.isLeft()) {
-			 System.out.println("Left Hand detected");
-			 //System.out.print("Position: "+position+", Velocity: "+velocity+" ,Direction: "+direction);
-			 //System.out.println("Roll: "+roll);
-			 System.out.println("Pitch: "+ Math.toDegrees(pitch));
-		 }
-		 else{
-			 System.out.println("Right Hand detected");
-		 }
-		 
-		 /*System.out.println("Frame id: " + frame.id()
-         + ", timestamp: " + frame.timestamp()
-         + ", hands: " + frame.hands().count()
-         + ", fingers: " + frame.fingers().count());*/
+		 if (hands.isEmpty())
+			 System.out.println("Blank!");
+		 else {
+			 Hand firstHand = hands.get(0);
+			 
+			//get fingers
+			 FingerList fingers = firstHand.fingers();
+			 
+			 //get position, velocity and direction
+			 //Vector position = firstHand.palmPosition();
+			 //Vector velocity = firstHand.palmVelocity();
+			 //Vector direction = firstHand.direction();
+			 
+			 float pitch = firstHand.direction().pitch();
+			 float yaw = firstHand.direction().yaw();
+			 float roll = firstHand.palmNormal().roll();
+			 if (firstHand.isLeft()) {
+				 System.out.println("Left Hand detected");
+				 //System.out.print("Position: "+position+", Velocity: "+velocity+" ,Direction: "+direction);
+				 //System.out.println("Roll: "+roll);
+				 System.out.println("Pitch: "+ Math.toDegrees(pitch));
+			 }
+			 
+			 else{
+				 System.out.println("Right Hand detected");
+				 System.out.println("Pitch: "+ Math.toDegrees(pitch));
+			 }
+			 
+			 
+			 /*System.out.println("Frame id: " + frame.id()
+	         + ", timestamp: " + frame.timestamp()
+	         + ", hands: " + frame.hands().count()
+	         + ", fingers: " + frame.fingers().count());*/
 	    }
+	 }
 }
